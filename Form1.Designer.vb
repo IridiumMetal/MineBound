e8f79bedee9432fbafc42a4f07f63a4428bb948c
Binaried <YZX7222512@163.com>
2019-03-24 00:28:21 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "分辨率：N/A"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "宽高比：N/A"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 50)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 12)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "刷新率：N/A"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 70)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 12)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "色深度：N/A"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(13, 120)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(108, 16)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "重启资源管理器"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(13, 140)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(108, 16)
+        Me.CheckBox2.TabIndex = 6
+        Me.CheckBox2.Text = "自动应用分辨率"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 160)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(138, 24)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "应用分辨率"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.Maroon
+        Me.Label5.Location = New System.Drawing.Point(4, 190)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(149, 12)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "*关闭即可自动还原分辨率*"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.Color.DimGray
+        Me.Label6.Location = New System.Drawing.Point(7, 208)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(143, 12)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "*Powered by 天竺金平府*"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DisplayMember = "0"
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.ItemHeight = 12
+        Me.ComboBox1.Items.AddRange(New Object() {"1920x1080 32bit (16:9)", "1600x900 32bit (16:9)", "1280x720 32bit (16:9)", "1152x864 32bit (4:3)", "1024x768 32bit (4:3)", "800x600 32bit (4:3)", "640x480 32bit (4:3)"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 92)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(138, 20)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(158, 227)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "MineBound Beta"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,129 @@
+﻿Imports System.Runtime.InteropServices
+Public Class Form1
+    Dim Original, Current As New DEVMODE With {.dmSize = CShort(Marshal.SizeOf(GetType(DEVMODE)))}
+    Declare Function SetForegroundWindow Lib "user32" (ByVal hWnd As IntPtr) As IntPtr
+    Declare Function FindWindowA Lib "user32" (ByVal lpClassName As String, ByVal lpWindowName As String) As IntPtr
+    Declare Function GetWindowThreadProcessId Lib "user32" (ByVal hwnd As IntPtr, ByRef lpdwProcessId As IntPtr) As IntPtr
+    Declare Function GetPrivateProfileString Lib "kernel32" Alias "GetPrivateProfileStringA" (ByVal lpApplicationName As String, ByVal lpKeyName As String, ByVal lpDefault As String, ByVal lpReturnedString As System.Text.StringBuilder, ByVal nSize As Integer, ByVal lpFileName As String) As Long
+    Declare Function WritePrivateProfileString Lib "kernel32" Alias "WritePrivateProfileStringA" (ByVal lpApplicationName As String, ByVal lpKeyName As String, ByVal lpString As String, ByVal lplFilename As String) As Boolean
+#Region "DisplaySettings API"
+    <StructLayout(LayoutKind.Sequential, CharSet:=CharSet.Auto)> _
+    Public Structure DEVMODE
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=32)> _
+        Public dmDeviceName As String
+        Public dmSpecVersion As Short
+        Public dmDriverVersion As Short
+        Public dmSize As Short
+        Public dmDriverExtra As Short
+        Public dmFields As Integer
+        Public dmOrientation As Short
+        Public dmPaperSize As Short
+        Public dmPaperLength As Short
+        Public dmPaperWidth As Short
+        Public dmScale As Short
+        Public dmCopies As Short
+        Public dmDefaultSource As Short
+        Public dmPrintQuality As Short
+        Public dmColor As Short
+        Public dmDuplex As Short
+        Public dmYResolution As Short
+        Public dmTTOption As Short
+        Public dmCollate As Short
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=32)> _
+        Public dmFormName As String
+        Public dmLogPixels As Short
+        Public dmBitsPerPel As Integer
+        Public dmPelsWidth As Integer
+        Public dmPelsHeight As Integer
+        Public dmDisplayFlags As Integer
+        Public dmDisplayFrequency As Integer
+    End Structure
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)> _
+    Private Shared Function ChangeDisplaySettings(<[In]()> ByRef lpDevMode As DEVMODE, ByVal dwFlags As Integer) As Integer
+    End Function
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)> _
+    Private Shared Function EnumDisplaySettings(ByVal lpszDeviceName As String, ByVal iModeNum As Integer, ByRef lpDevMode As DEVMODE) As Boolean
+    End Function
+#End Region
+    Private Sub SettingsChanged() '配置项改变事件
+        WritePrivateProfileString("Settings", "Bound", ComboBox1.SelectedIndex, Environment.CurrentDirectory & "\MineBound.ini")
+        WritePrivateProfileString("Settings", "Refresh", CheckBox1.Checked, Environment.CurrentDirectory & "\MineBound.ini")
+        WritePrivateProfileString("Settings", "Auto", CheckBox2.Checked, Environment.CurrentDirectory & "\MineBound.ini")
+    End Sub
+    Private Sub ActivationChanged() '激活状态改变事件
+        EnumDisplaySettings(My.Computer.Screen.DeviceName, 0, Current)
+        With Current
+            .dmPelsWidth = My.Computer.Screen.Bounds.Width
+            .dmPelsHeight = My.Computer.Screen.Bounds.Height
+            .dmBitsPerPel = My.Computer.Screen.BitsPerPixel
+            Label1.Text = "分辨率：" & .dmPelsWidth & "x" & .dmPelsHeight
+            Label2.Text = "宽高比：" & IIf(.dmPelsHeight / .dmPelsWidth * 16 = 12, "4:3", "16:" & .dmPelsHeight / .dmPelsWidth * 16) ' y/x * 16
+            Label3.Text = "刷新率：" & .dmDisplayFrequency & "Hz"
+            Label4.Text = "色深度：" & .dmBitsPerPel & "Bit"
+        End With
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Icon = My.Resources.BnB_MineSweeper
+        If System.IO.File.Exists("MineBound.ini") Then
+            Dim tString As New System.Text.StringBuilder
+            GetPrivateProfileString("Settings", "Bound", -1, tString, 255, Environment.CurrentDirectory & "\MineBound.ini")
+            ComboBox1.SelectedIndex = tString.ToString
+            GetPrivateProfileString("Settings", "Refresh", False, tString, 255, Environment.CurrentDirectory & "\MineBound.ini")
+            CheckBox1.Checked = Boolean.Parse(tString.ToString)
+            GetPrivateProfileString("Settings", "Auto", False, tString, 255, Environment.CurrentDirectory & "\MineBound.ini")
+            CheckBox2.Checked = Boolean.Parse(tString.ToString)
+        End If '读取配置文件
+        AddHandler CheckBox1.CheckedChanged, AddressOf SettingsChanged
+        AddHandler CheckBox2.CheckedChanged, AddressOf SettingsChanged
+        AddHandler ComboBox1.SelectedIndexChanged, AddressOf SettingsChanged
+        AddHandler Activated, AddressOf ActivationChanged
+        AddHandler Deactivate, AddressOf ActivationChanged
+        '绑定事件处理函数
+    End Sub
+    Private Sub Form1_Shown(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Shown
+        Original = Current '置原始环境数据
+        If CheckBox2.Checked Then
+            Button1_Click(Nothing, Nothing)
+        End If '自动应用分辨率
+        If System.IO.File.Exists("ms_arbiter.exe") AndAlso FindWindowA("TMain", "Minesweeper Arbiter ").ToInt32 = 0 Then
+            Process.Start("ms_arbiter.exe")
+        End If '启动扫雷
+    End Sub
+    Private Sub Form1_Closed(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles MyBase.FormClosed
+        ChangeDisplaySettings(Original, 0) '还原
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If CheckBox1.Checked Then
+            Dim tProcId As New IntPtr
+            GetWindowThreadProcessId(FindWindowA("Shell_TrayWnd", Nothing), tProcId)
+            Process.GetProcessById(tProcId).Kill()
+        End If '重启资源管理器
+        With Current
+            Select Case ComboBox1.SelectedIndex
+                Case 0
+                    .dmPelsWidth = 1920
+                    .dmPelsHeight = 1080
+                Case 1
+                    .dmPelsWidth = 1600
+                    .dmPelsHeight = 900
+                Case 2
+                    .dmPelsWidth = 1280
+                    .dmPelsHeight = 720
+                Case 3
+                    .dmPelsWidth = 1152
+                    .dmPelsHeight = 864
+                Case 4
+                    .dmPelsWidth = 1024
+                    .dmPelsHeight = 768
+                Case 5
+                    .dmPelsWidth = 800
+                    .dmPelsHeight = 600
+                Case 6
+                    .dmPelsWidth = 640
+                    .dmPelsHeight = 480
+            End Select '变更边界参数
+        End With
+        ChangeDisplaySettings(Current, 0) '变更屏幕边界
+        SetForegroundWindow(FindWindowA("TMain", "Minesweeper Arbiter ")) '呼出扫雷
+    End Sub
+End Class
